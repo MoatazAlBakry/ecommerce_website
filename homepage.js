3d5569285b4c5e7a84661746a6542606d1bbb131
Moataz AlBakry <MoatazTamerBusiness@gmail.com>
2025-02-10 18:56:10 +0200

    finalised dark mode

--- homepage.js
+++ homepage.js
@@ -1,13 +1,30 @@
 const mobile = document.querySelector('.mobile');
 const navLinks = document.querySelector('.nav_links');
-
 mobile.addEventListener('click', () => {
     navLinks.classList.toggle('active');
 });
-
 const navItems = document.querySelectorAll('.nav_links li');
 navItems.forEach(item => {
     item.addEventListener('click', () => {
-        navLinks.classList.remove('active'); // Hide the navigation menu
+        navLinks.classList.remove('active');
     });
+});
+// dark mode
+const darkModeToggle = document.querySelector('.fa-moon');
+const body = document.body;
+
+//user preference
+const savedTheme = localStorage.getItem('theme');
+if (savedTheme === 'dark') {
+    body.classList.add('dark-mode');
+}
+
+// Toggle event listener & save user pref
+darkModeToggle.addEventListener('click', () => {
+    body.classList.toggle('dark-mode');
+    if (body.classList.contains('dark-mode')) {
+        localStorage.setItem('theme', 'dark');
+    } else {
+        localStorage.setItem('theme', 'light');
+    }
 });
